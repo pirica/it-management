3c04f6fb89a6717326bffb3e47c27e76f5eccefc
Nelson Salvador <nelson.salvador@gmail.com>
2026-04-02 03:41:07 +0100

    Update index.php

--- modules/user_companies/index.php
+++ modules/user_companies/index.php
@@ -158,7 +158,7 @@ function cr_format_company_list($companies) {
     $lines = [
         sanitize($companies[0]) . ' - ' . sanitize($companies[1]),
     ];
-    for ($i = 2; $i < $count; $i++) {
+    for ($i = 1; $i < $count; $i++) {
         $lines[] = '- ' . sanitize($companies[$i]);
     }
 
