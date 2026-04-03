09949837c95c817b5f5610f760725be79c0380f8
Nelson Salvador <nelson.salvador@gmail.com>
2026-04-04 00:39:30 +0100

    Update ui_config.php

--- includes/ui_config.php
+++ includes/ui_config.php
@@ -21,7 +21,7 @@ function itm_sidebar_base_structure() {
                 ['id' => 'printers', 'label' => '🖨️ Is Printer', 'href' => 'modules/printers/', 'match_dir' => 'printers'],
                 ['id' => 'pos', 'label' => '🏧 Is POS', 'href' => 'modules/pos/', 'match_dir' => 'pos'],
                 ['id' => 'switch_ports', 'label' => '🖧 Switch Ports', 'href' => 'modules/switch_ports/', 'match_dir' => 'switch_ports'],
-                ['id' => 'tickets', 'label' => '🎫 Tickets', 'href' => 'modules/tickets/', 'match_dir' => 'tickets'],
+                ['id' => 'tickets', 'label' => '🎟️ Tickets', 'href' => 'modules/tickets/', 'match_dir' => 'tickets'],
             ],
         ],
         [
