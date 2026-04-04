cbec3eb4f8500c4a939e9fd52b4eda88366a0086
Nelson Salvador <nelson.salvador@gmail.com>
2026-04-04 01:21:16 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -124,7 +124,7 @@ if ($userDisplayName !== '' && $userEmail !== '') {
         <div class="main-content">
             <?php include 'includes/header.php'; ?>
 
-<!--            <div> -->
+
             <div  class="content" style="position:relative;display:flex;justify-content:flex-end;align-items:center;margin-bottom:20px;min-height:40px;">
                 <h1 style="position:absolute;left:50%;transform:translateX(-50%);margin:0;text-align:center;">📊 Dashboard</h1>
                 
