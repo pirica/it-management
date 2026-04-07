e8522fcba26153298277a54b722cc2a92401197a
Nelson Salvador <nelson.salvador@gmail.com>
2026-04-07 03:28:46 +0100

    Delete modules/printers directory

--- modules/printers/view.php
+++
@@ -1,6 +0,0 @@
-<?php
-$equipmentViewTitle = 'View Printer';
-$equipmentRequiredFlagField = 'is_printer';
-$equipmentViewBackPath = 'index.php';
-$equipmentViewEditPath = '../equipment/edit.php';
-require '../equipment/view.php';
