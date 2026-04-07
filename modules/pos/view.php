4817e14882d3d55f3848aea3dad1894b97c72cad
Nelson Salvador <nelson.salvador@gmail.com>
2026-04-07 03:28:33 +0100

    Delete modules/pos directory

--- modules/pos/view.php
+++
@@ -1,6 +0,0 @@
-<?php
-$equipmentViewTitle = 'View POS';
-$equipmentRequiredFlagField = 'is_pos';
-$equipmentViewBackPath = 'index.php';
-$equipmentViewEditPath = '../equipment/edit.php';
-require '../equipment/view.php';
