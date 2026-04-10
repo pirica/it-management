b3c2bcffd493fe3b9502daf42071508d929322de
Nelson Salvador <nelson.salvador@gmail.com>
2026-04-10 15:23:22 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -135,7 +135,7 @@ if ($userDisplayName !== '' && $userEmail !== '') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - IT Management</title>
+    <title>Dashboard - ⚙️ IT Controls</title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
