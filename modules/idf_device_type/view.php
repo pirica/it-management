fccdbb9abd6f3f52a8352bf09b49c70a34ae1fa6
Nelson Salvador <nelson.salvador@gmail.com>
2026-04-12 01:28:59 +0100

    Add idf_device_type CRUD module scaffold

---
+++ modules/idf_device_type/view.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * IDF Device Type Module - View
+ *
+ * Wrapper for the local CRUD implementation.
+ */
+
+$crud_table = 'idf_device_type';
+$crud_title = 'IDF Device Type';
+$crud_action = 'view';
+require 'index.php';
