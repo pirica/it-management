f96c18899f527ad458a93d3d1b6bc06a54fa62a2
Nelson Salvador <nelson.salvador@gmail.com>
2026-04-22 00:52:01 +0100

    Rename Chart of Accounts label to GL Accounts

--- modules/gl_accounts/view.php
+++ modules/gl_accounts/view.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * 📚 Chart of Accounts Module - View
+ * 📚 GL Accounts Module - View
  * 
  * Wrapper for the master CRUD implementation.
  * Configures the action to 'view' and delegates to index.php.
  */
 
 $crud_table = 'gl_accounts';
-$crud_title = '📚 Chart of Accounts';
+$crud_title = '📚 GL Accounts';
 $crud_action = 'view';
 require 'index.php';
