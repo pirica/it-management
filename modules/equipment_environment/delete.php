c6afba58bcd709150f11a6a2e9b22e73ee0f5acc
Nelson Salvador <nelson.salvador@gmail.com>
2026-04-27 20:14:58 +0100

    Add missing equipment_environment CRUD module

---
+++ modules/equipment_environment/delete.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Equipment Environment Module - Delete
+ * 
+ * Wrapper for the local CRUD implementation.
+ */
+
+$crud_table = 'equipment_environment';
+$crud_title = 'Equipment Environment';
+$crud_action = 'delete';
+require 'index.php';
