7c45776400d3a1196c58c1c3f62009e3712a9c18
Nelson Salvador <nelson.salvador@gmail.com>
2026-05-07 19:51:27 +0100

    Force utf8mb4 session charset in database dump

--- database.sql
+++ database.sql
@@ -4,6 +4,9 @@
 DROP DATABASE IF EXISTS `itmanagement`;
 CREATE DATABASE `itmanagement` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `itmanagement`;
+SET NAMES utf8mb4;
+SET CHARACTER SET utf8mb4;
+SET collation_connection = 'utf8mb4_unicode_ci';
 SET FOREIGN_KEY_CHECKS=0;
 
 
