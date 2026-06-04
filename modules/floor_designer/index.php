84f45852be5149e7448f4da895ff5eaba2e36856
Nelson Salvador <nelson.salvador@gmail.com>
2026-06-04 22:37:54 +0100

    Update index.php

--- modules/floor_designer/index.php
+++ modules/floor_designer/index.php
@@ -263,18 +263,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ajax_action'])) {
             $sql = "UPDATE floor_designer_points SET point_type_id=?, x=?, y=?, comment_x=?, comment_y=?, label=?, rotation=?, wlan_address=?, ip_address=?, mac_address=?, patch_port=?, switch_id=?, switch_port_id=?, cable_color_id=? WHERE id=? AND company_id=?";
             $stmt = mysqli_prepare($conn, $sql);
             mysqli_stmt_bind_param($stmt, 'iddddsdssssiiiii', $point_type_id, $x, $y, $comment_x, $comment_y, $label, $rotation, $wlan_address, $ip_address, $mac_address, $patch_port, $switch_id, $switch_port_id, $cable_color_id, $point_id, $company_id);
-        );
-
-        
-        
         } else {
             $sql = "INSERT INTO floor_designer_points (company_id, floor_designer_id, point_type_id, x, y, comment_x, comment_y, label, rotation, wlan_address, ip_address, mac_address, patch_port, switch_id, switch_port_id, cable_color_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             $stmt = mysqli_prepare($conn, $sql);
             mysqli_stmt_bind_param($stmt, 'iiiddddsdssssiii', $company_id, $floor_designer_id, $point_type_id, $x, $y, $comment_x, $comment_y, $label, $rotation, $wlan_address, $ip_address, $mac_address, $patch_port, $switch_id, $switch_port_id, $cable_color_id);
-        );
-        
-        
-        
         }
 
         if (mysqli_stmt_execute($stmt)) {
